a82b6378388e1023d46d099dc1759df5dc995555
Kim DaeHan <dhkim@day1ent.com>
2023-09-22 14:10:09 +0900

    .

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"log"
+
 	"github.com/Kim-DaeHan/my-gin-app/config"
+	"github.com/joho/godotenv"
 
 	"github.com/Kim-DaeHan/my-gin-app/routes"
 
@@ -9,6 +12,12 @@ import (
 )
 
 func main() {
+
+	// .env 파일 로딩
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	router := gin.Default()
 
 	//run database
